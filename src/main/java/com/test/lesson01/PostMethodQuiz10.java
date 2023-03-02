ff5bc4ba67c97a8949c71e9affb044386dc64c6b
DESKTOP-I7UUISV\user <test@test.com>
2023-03-02 20:22:26 +0900

    내용 수정

--- src/main/java/com/test/lesson01/PostMethodQuiz10.java
+++ src/main/java/com/test/lesson01/PostMethodQuiz10.java
@@ -11,33 +11,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/lesson01/quiz10")
-
-
-public class PostMethodQuiz10 extends HttpServlet{
-	
-	// doPost 메소드 바깥쪽에 위치
+public class PostMethodQuiz10 extends HttpServlet {
 	private final Map<String, String> userMap =  new HashMap<String, String>() {
-	    {
-	        put("id", "marobiana");
-	        put("password", "qwerty1234");
-	        put("name", "신보람");
-	    }
+		{
+			put("id", "marobiana");
+			put("password", "qwerty1234");
+			put("name", "신보람");
+		}
 	};
-	@Override
+	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.setContentType("text/html");
-		String userId = request.getParameter("userId");
+		//response.setCharacterEncoding("utf-8");   filter
+		
+		String id = request.getParameter("id");
 		String password = request.getParameter("password");
+		
 		PrintWriter out = response.getWriter();
-		
-		// 주석 추가
-		
-		if(userId.equals(userMap.get("id"))) {
-			out.print("아이디가 일치하지 않습니다");
-		}else if(password.equals(userMap.get("password"))) {
-			out.print("비밀번호가 일치하지 않습니다.");
-		}else {
-			out.print(userMap.get("name") + "님 환영합니다!");
+		out.print("<html><head><title>결과</title></head><body>");
+		if (userMap.get("id").equals(id) == false) {
+			out.print("id가 일치하지 않습니다.");
+		} else if (userMap.get("password").equals(password) == false) {
+			out.print("password가 일치하지 않습니다.");
+		} else {
+			out.print(userMap.get("name") + "님 환영합니다!!!");
 		}
+		out.print("</body></html>");
 	}
 }
